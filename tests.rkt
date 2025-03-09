2b8f7ab1cd83dd79f0cdcfb4bf9a116280da8816
Jonas Brüll <jonas.bruell@gmail.com>
2025-03-09 16:03:24 +0100

    Aanpassen testing suite met gui startup wizzard

--- tests.rkt
+++ tests.rkt
@@ -18,10 +18,7 @@
 ;; What about de twee wissels in een lijst steken als connections??
 ;; -> ook voor testing accessible??
 
-(require rackunit
-         rackunit/gui
-         rackunit/text-ui
-         racket/cmdline
+(require racket/gui racket/cmdline rackunit rackunit/gui rackunit/text-ui
          ;; RAILWAY
          "railway/crossing.test.rkt"
          "railway/light.test.rkt"
@@ -37,40 +34,75 @@
          ;; PROVIDER
          )
 
-(provide all-tests)
-
 ; running all test suites
-(define all-tests
-  (test-suite "Unit testing all modules"
-              ;; RAILWAY
-              (test-suite "Unit testing of all RAILWAY module operations"
-                          crossing-test
-                          light-test
-                          segment-test
-                          detection-block-test
-                          switch-test
-                          ;switch-3way-test
-                          ;switch-cross-test
-                          ;train-test
-                          ;main-test
-                          )
-              ;; INFRABEL
-              (test-suite "Unit testing of all INFRABEL module operations"
-                          ;(track-interface-test 'sim)
-                          (test-case "test"
-                                     (check-not-exn (λ () '())))
-                          )
-              ;; PROVIDER
-              (test-suite "Unit testing of all PROVIDER module operations"
-                          (test-case "test"
-                                     (check-not-exn (λ () '())))
-                          )
-              ))
+(define (all-tests sim? gui?)
+  (test-suite
+   "Unit testing all modules"
+   ;; TRACK
+   (test-suite "Unit testing of all TRACK module operations"
+               (test-case "test"
+                          (check-not-exn (λ () '())))
+               )
+   ;; RAILWAY
+   (test-suite "Unit testing of all RAILWAY module operations"
+               crossing-test
+               light-test
+               segment-test
+               detection-block-test
+               switch-test
+               ;switch-3way-test
+               ;switch-cross-test
+               ;train-test
+               ;main-test
+               )
+   ;; INFRABEL
+   (test-suite "Unit testing of all INFRABEL module operations"
+               ;(track-interface-test 'sim)
+               (test-case "test"
+                          (check-not-exn (λ () '())))
+               )
+   ;; PROVIDER
+   (test-suite "Unit testing of all PROVIDER module operations"
+               (test-case "test"
+                          (check-not-exn (λ () '())))
+               )))
+
+;; RackUnit setup GUI for manual running of program
+(define gui%
+  (class dialog%
+    (init-field startup-callback)
+    (super-new (label "RackUnit setup"))
+    (let ((config-pane
+           (new vertical-pane% (parent this) (alignment '(left bottom))))
+          (start-pane
+           (new pane% (parent this) (alignment '(center center))))
+          (sim? #t))
+      (new check-box%
+           (label "Test GUI components") (vert-margin 10) (horiz-margin 10)
+           (parent config-pane)
+           (value #t)
+           (callback (λ (t e) (set! sim? (send t get-value)))))
+      (new check-box%
+           (label "Test SIM components xxxxxxxxxxxx") (horiz-margin 10)
+           (parent config-pane)
+           (value #t)
+           (callback (λ (t e) (set! sim? (send t get-value)))))
+      (new button%
+           (label "start") (vert-margin 10)
+           (parent start-pane)
+           (callback (λ (t e) (send this show #f) (startup-callback sim?)))))))
+
+(define startup-callback
+  (λ (sim?)
+    (test/gui (all-tests sim? #t))))
+
+;; Manual running or automatic testing with "-g" flag
 (let/cc exit
-(command-line
- #:once-any
- [("-g" "--github") "Run tests without GUI"
-                    (run-tests all-tests 'verbose)
-                    (exit)]
- #:args ()
- (test/gui all-tests)))
+  (command-line
+   #:once-any
+   [("-g" "--github")
+    "Run tests without GUI"
+    (run-tests (all-tests #t #f) 'verbose)
+    (exit)]
+   #:args ()
+   (send (make-object gui% startup-callback) show #t)))
